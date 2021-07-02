667055c14c774405b0e8fa0bb4367548b92e9815
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-07-02 16:25:52 +0300

    add buildMenu

---
+++ src/js/build-menu.js
@@ -0,0 +1,10 @@
+import refs from '../js/refs';
+import menu from '../data/menu.json';
+import menuListTemplate from '../template/menuListTemplate';
+
+const buildMenu = () => {
+  refs.menuList.insertAdjacentHTML('beforeend', menuListTemplate(menu));
+};
+
+export default buildMenu();
+
